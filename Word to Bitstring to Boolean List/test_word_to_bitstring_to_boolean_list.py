4139057f125ae54aa6d8181b1557dae8168b199d
Andrzej Stecyk <andrzej.stecyk.to.ja@gmail.com>
2021-03-10 18:57:47 +0100

    to_boolean_list

---
+++ Word to Bitstring to Boolean List/test_word_to_bitstring_to_boolean_list.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from word_to_bitstring_to_boolean_list import to_boolean_list
+
+
+class Test(TestCase):
+    def test_to_boolean_list(self):
+        actual_param = [
+            "charming", "exquisite", "admire", "deep", "loves", "tesh",
+            "xavier", "adores", "tesha", "esquire", "randomize", "exotic"
+        ]
+        expected_param = [
+            [True, False, True, False, True, True, False, True],
+            [True, False, True, True, True, True, True, False, True],
+            [True, False, True, True, False, True],
+            [False, True, True, False],
+            [False, True, False, True, True],
+            [False, True, True, False],
+            [False, True, False, True, True, False],
+            [True, False, True, False, True, True],
+            [False, True, True, False, True],
+            [True, True, True, True, True, False, True],
+            [False, True, False, False, True, True, True, False, True],
+            [True, False, True, False, True, True]
+        ]
+        for i, x in enumerate(actual_param): self.assertEquals(to_boolean_list(x), expected_param[i])
